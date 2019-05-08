7c50138495be2d1023459f11d0b1216c5eb30bf1
Ashley Carmichael Whitt <ashley_marlaine@Ashleys-MacBook-Air.local>
2019-05-08 09:48:13 -0400

    May 8 AM push no real changes

--- src/app/material.ts
+++ src/app/material.ts
@@ -4,8 +4,8 @@ import { MatToolbarModule} from '@angular/material';
 import { MatIconModule} from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-    exports:  [MatButtonModule, MatCheckboxModule]
+    imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
+    exports:  [MatButtonModule, MatCheckboxModule, MatIconModule]
 })
 
 export class MaterialModule{}
