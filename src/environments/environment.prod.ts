3ec376ba0a6f66b1dfc8553ae6b2ee9e939df839
Becky <you@example.com>
2019-05-17 10:02:03 -0400

    asda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
-  production: true
-};
+export let APIURL = '';
+
+switch (window.location.hostname) {
+  case 'fitness-together-client.heroku.com':
+    APIURL = 'https://fitness-together.heroku.com'
+    break;
+  default:
+    APIURL = 'http://localhost:3000';
+}
